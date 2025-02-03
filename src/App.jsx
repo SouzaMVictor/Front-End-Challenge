2ecc2c6092f2e58bab529c52bf9c110e56e88bb6
SouzaMVictor <souzamvictor@gmail.com>
2025-02-03 16:57:43 -0300

    static header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { Header } from "./Header";
 import "./App.css";
+import { Hero } from "./Hero.1";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,53 @@
+import MobileMenu from "./assets/Icon.svg";
+import LogoCoreBiz from "./assets/site-logo-corebiz-preto-cinza.png";
+import ShoppingCart from "./assets/shopping-cart 1.png";
+import Lupa from "./assets/Vector.png";
+import { useState } from "react";
 export function Header() {
   return (
-    <header className="bg-yellow-300 text-3xl text-blue-600">
-      hello world
+    // global container
+    <header>
+      <div className="flex justify-around pt-4">
+        {/* items container */}
+        <MenuMobile />
+        <CoreBizLogo />
+        <Cart />
+      </div>
+      <InputProcura />
     </header>
+  );
+}
+function MenuMobile() {
+  return <img src={MobileMenu} alt="menu mobile" />;
+}
+function CoreBizLogo() {
+  return <img src={LogoCoreBiz} alt="logo da empresa CoreBiz" />;
+}
+function Cart() {
+  return (
+    <img
+      src={ShoppingCart}
+      alt="carrinho de compras"
+      className="h-[18px] w-[18px]"
+    />
+  );
+}
+
+function InputProcura() {
+  const [query, setQuery] = useState("");
+  return (
+    <div className="relative mx-[20px] my-2">
+      <input
+        type="text"
+        placeholder="O que está procurando?"
+        className="w-full border-b border-gray-400 py-1 pr-10 pl-2 text-[13px] text-gray-600 focus:border-black focus:outline-none"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <img
+        src={Lupa}
+        alt="Procurar"
+        className="absolute top-1/2 right-2 h-5 w-5 -translate-y-1/2 transform text-gray-600"
+      />
+    </div>
   );
 }
