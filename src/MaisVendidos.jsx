3b9f33a02eb868067c621e8f435ed420e3f0cb5a
SouzaMVictor <souzamvictor@gmail.com>
2025-02-04 03:15:16 -0300

    conditional render nas informações da api e estilização básica dos componentes

--- src/MaisVendidos.jsx
+++ src/MaisVendidos.jsx
@@ -17,7 +17,7 @@ export function MaisVendidos() {
         <h2 className="ml-[28px] font-black">Mais Vendidos</h2>
         <div className="ml-[28px] h-1 w-10 bg-[#C0C0C0C0]"></div>
       </div>
-      <ul className="grid grid-cols-2 gap-4">
+      <ul className="mx-3 grid grid-cols-2 gap-4">
         {products.map((products) => (
           <ProductItems products={products} key={products.productId} />
         ))}
@@ -26,35 +26,49 @@ export function MaisVendidos() {
   );
 }
 function ProductItems({ products }) {
+  const hasInstallments =
+    products.installments && products.installments.length > 0;
+  const installmentInfo = hasInstallments ? products.installments[0] : null;
   return (
-    <li>
-      <img src={products.imageUrl} alt={products.productName} loading="lazy" />
-      <p className="text-cinza">{products.productName}</p>
-      <span>{products.stars}</span>
+    <li className="group bg-white text-center duration-300 hover:bg-gray-300">
+      <img
+        src={products.imageUrl}
+        alt={products.productName}
+        loading="lazy"
+        className="mx-auto"
+      />
+      <p className="text-cinza text-[10px]">{products.productName}</p>
+      {products.stars ? <span>{products.stars}</span> : <span>&nbsp;</span>}
       {products.listPrice ? (
-        <p className="text-cinza line-through">
+        <p className="text-cinza text-[11px] line-through">
           de R${(products.listPrice / 100).toFixed(2).replace(".", ",")}
+        </p>
+      ) : (
+        <p className="text-[11px]">&nbsp;</p>
+      )}
+      <p className="text-[16px] font-bold">
+        por R${(products.price / 100).toFixed(2).replace(".", ",")}
+      </p>
+      {installmentInfo ? (
+        <p className="text-cinza text-[11px]">
+          ou &nbsp;
+          {products.installments.map((installment) => (
+            <span key={products.productName}>
+              {installment.quantity}x de R$
+              {(installment.value / 100).toFixed(2).replace(".", ",")}
+            </span>
+          ))}
         </p>
       ) : (
         <p>&nbsp;</p>
       )}
-      <p>por R${(products.price / 100).toFixed(2).replace(".", ",")}</p>
-      <p className="text-cinza">
-        ou &nbsp;
-        {products.installments.map((installment) => (
-          <span key={products.productName}>
-            {installment.quantity}x de R$
-            {(installment.value / 100).toFixed(2).replace(".", ",")}
-          </span>
-        ))}
-      </p>
       <BotaoComprar />
     </li>
   );
 }
 function BotaoComprar() {
   return (
-    <button className="rounded-lg bg-black px-8 py-3 font-bold text-white uppercase">
+    <button className="rounded-lg bg-black px-8 py-3 text-[15px] font-bold text-white uppercase duration-300 hover:bg-neutral-800">
       comprar
     </button>
   );
