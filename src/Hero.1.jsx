aa3e17d49fcf590f3a8976679a0f4a2af607d1eb
SouzaMVictor <souzamvictor@gmail.com>
2025-02-05 13:43:28 -0300

    implementação da biblioteca useMediaQuery nos banners, para exibi-los dinamicamente. adicionado banners desktop

--- src/Hero.1.jsx
+++ src/Hero.1.jsx
@@ -1,13 +1,22 @@
 import Banner1 from "./assets/mob0.png";
 import Banner2 from "./assets/mob1.png";
 import Banner3 from "./assets/mob2.png";
+import BannerDesk1 from "./assets/Banner (1).jpg";
+import BannerDesk2 from "./assets/desk1.png";
+import BannerDesk3 from "./assets/desk2.png";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
-const images = [Banner1, Banner2, Banner3];
+import { useMediaQuery } from "react-responsive";
+
 export function Hero() {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const images = isMobile
+    ? [Banner1, Banner2, Banner3]
+    : [BannerDesk1, BannerDesk2, BannerDesk3];
+
   return (
     <Swiper
       slidesPerView={1}
@@ -17,12 +26,12 @@ export function Hero() {
       modules={[Navigation, Pagination]}
       className="h-auto w-full"
     >
-      {images.map((image, index) => (
+      {images.map((images, index) => (
         <SwiperSlide key={index} className="flex justify-center">
           <img
-            src={image}
+            src={images}
             alt={`Banner ${index + 1}`}
-            className="h-auto w-full object-cover"
+            className="h-auto w-full object-cover lg:h-[430px]"
           />
         </SwiperSlide>
       ))}
